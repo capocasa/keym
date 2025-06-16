7cd9bbcba349e9dfcbbe903323e637e26e41f3ec
Carlo Capocasa <carlo@capocasa.net>
2025-06-16 14:17:37 +0200

    use JACK midi

--- keym.nim
+++ keym.nim
@@ -1,20 +1,48 @@
 import std/[monotimes]
-import rtthread, jill, jill/ringbuffer, jill/os, keym/codes, jacket
+import rtthread, jill/ringbuffer, jill/os, keym/codes, jacket
+from posix import Timeval, Timespec, clock_gettime, CLOCK_MONOTONIC
 
 type
-  KernelTime {.packed.} = object
-    sec: int64
-    nsec: int64
   RawKeyboardEvent {.packed.} = object
-    time: KernelTime
+    time: Timeval
     kind: uint16
     code: uint16
     value: int32
-
   KeyboardEvent {.packed.} = object
-    usec: uint64
+    usec: int64
     note: int8
     on: bool
+  MidiWriterState = object  # this gets passed into jack callback
+    count: int
+    jackTimeUsec: int64
+    sysTimeUsec: int64
+    sysTimeOffsetUsec: int64
+
+let
+  EVIOCSCLOCKID* {.importc, header: "<linux/input.h>".}: culong
+
+proc toUsec(t: Timeval): int64 =
+  ## older linux kernel time format with microseconds
+  ## used by event subsystem
+  t.tv_sec.int64 * 1_000_000 + t.tv_usec.int64
+
+proc toUsec(t: Timespec): int64 =
+  ## newer linux kernel time format with nanoseconds
+  ## used to retrieve system time
+  t.tv_sec.int64 * 1_000_000 + t.tv_nsec.int64 div 1000
+
+proc getSysTimeUsec(): int64 =
+  var ts {.noinit.}: Timespec
+  discard clock_gettime(CLOCK_MONOTONIC, ts)
+  ts.toUsec
+
+proc ioctl(f: FileHandle, request: culong, arg: pointer): cint {.importc: "ioctl", header: "<sys/ioctl.h>".}
+  ## std/posix ioctl signature doesn't match
+
+proc openDevice(path: string): File =
+  result = open(path)
+  var clk = CLOCK_MONOTONIC
+  assert ioctl(result.getOSFileHandle, EVIOCSCLOCKID, clk.addr) == 0, "Could not set input subsystem to monotonic time"
 
 proc scancodeToNote(scancode: uint16, note: var int8): bool =
   #echo "> ", scancode
@@ -103,9 +131,6 @@ proc scancodeToNote(scancode: uint16, note: var int8): bool =
     return false
   true
 
-proc toUsec(t: KernelTime): uint64 =
-  uint64(t.sec * 1_000_000 + t.nsec div 1000)
-
 const
   keyboardEventPriority = 96
   keyboardPath = "/dev/input/event3"
@@ -115,11 +140,13 @@ var
   signalThread:Thread[void]
   keyboardEventBuffer = newRingBuffer[KeyboardEvent](2048)
   terminating = false
-  jackTimeUsec = 0#jacket.getTime()
-  sysTimeUsec = uint64(getMonoTime().ticks div 1000)
+  midiPort: Port
+  midiWriterClient: Client
+  midiWriterStatus: cint
+  midiWriterState: MidiWriterState
 
 let
-  keyboardDevice = open(keyboardPath)
+  keyboardDevice = openDevice(keyboardPath)
 
 proc keyboardEventHandler() =
   while not terminating:
@@ -130,18 +157,45 @@ proc keyboardEventHandler() =
     if rawKeyboardEvent.kind == 1'u16:  # ensure keyboard events only
       case rawKeyboardEvent.value
       of 0'i32, 1'i32:  # press and release only
-        echo rawKeyboardEvent
         var keyboardEvent:KeyboardEvent
         if not scancodeToNote(rawKeyboardEvent.code, keyboardEvent.note):
           # undefined code, ignore
           continue
         keyboardEvent.usec = rawKeyboardEvent.time.toUsec
         keyboardEvent.on = rawKeyboardEvent.value.bool
-        echo keyboardEvent
         keyboardEventBuffer.push(keyboardEvent)
       else:
         # ignore repeat (which would be 2'i32)
         discard
+
+proc midiWriter*(nFrames: NFrames, arg: pointer): cint {.cdecl.} =
+  let state = cast[ptr MidiWriterState](arg)
+  let outbuf = portGetBuffer(midiPort, nFrames)
+  var
+    frames: uint32
+
+    currentEvent: KeyboardEvent
+    currentFrame: uint32
+    currentFrameUsec: int64
+    currentFrameUsecUnsigned: uint64
+    nextFrameUsec: int64
+    nextFrameUsecUnsigned: uint64
+    periodUsec: float32
+
+  if state.count == 0:
+    state.sysTimeUsec = getSysTimeUsec()
+    state.jackTimeUsec = jacket.getTime().int64
+    state.sysTimeOffsetUsec = state.sysTimeUsec - state.jackTimeUsec
+    state.count = 10
+  if getCycleTimes(midiWriterClient, frames.addr, currentFrameUsecUnsigned.addr, nextFrameUsecUnsigned.addr, periodUsec.addr) == 0:
+    currentFrameUsec = currentFrameUsecUnsigned.int64
+    nextFrameUsec = nextFrameUsecUnsigned.int64
+    if keyboardEventBuffer.pop(currentEvent):
+      echo $currentEvent
+      while currentEvent.usec < nextFrameUsec:
+        if currentEvent.usec >= currentFrameUsec:
+          discard
+        discard keyboardEventBuffer.pop(currentEvent)
 
 createThread signalThread, proc() {.thread.} =
   waitSignals(SIGABRT, SIGHUP, SIGINT, SIGQUIT, SIGTERM):
@@ -149,27 +203,54 @@ createThread signalThread, proc() {.thread.} =
 
 blockSignals(SIGABRT, SIGHUP, SIGINT, SIGQUIT, SIGTERM)
 
-createRealtimeThread(keyboardEventThread, keyboardEventHandler)
+midiWriterClient = clientOpen("keym", NoStartServer or UseExactName, midiWriterStatus.addr)
+assert not midiWriterClient.isNil, "Could not create jack client, jack may not be running"
+midiPort = midiWriterClient.portRegister("out", JackDefaultMidiType, PortIsOutput, 0)
+assert midiWriterClient.setProcessCallback(midiWriter, midiWriterState.addr) == 0, "could not set process callback"
 
-#setSignalProc(signalHandler, SIGABRT, SIGHUP, SIGINT, SIGQUIT, SIGTERM)
+assert midiWriterClient.activate() == 0, "Could not connect jack"
+
+createRealtimeThread(keyboardEventThread, keyboardEventHandler)
+joinThread(keyboardEventThread)  # exit when input thread does, it's simpler not to have to kill it
+
+#[
 
 withJack (), (foo), defaultClientName(), false:
   var
+    jackTimeUsec: int64 
+    sysTimeUsec: int64
+    sysTimeOffset: int64
+
     currentEvent: KeyboardEvent
-    frames: uint32
-    nextFrameUsec: uint64
-    currentFrameUsec: uint64
-    period: float32
-  if getCycleTimes(client, frames.addr, currentFrameUsec.addr, nextFrameUsec.addr, period.addr) == 0:
-    discard keyboardEventBuffer.pop(currentEvent)
-    #echo "> ",$currentEvent
-    while currentEvent.usec < nextFrameUsec:
-      if currentEvent.usec >= currentFrameUsec:
-        echo $currentEvent
-      discard keyboardEventBuffer.pop(currentEvent)
-      for i in 0..<64:
-        foo[i] = 0.0
-joinThread(keyboardEventThread)
+
+    currentFrame: uint32
+    currentFrameUsec: int64
+    currentFrameUsecUnsigned: uint64
+    nextFrameUsec: int64
+    nextFrameUsecUnsigned: uint64
+    periodUsec: float32
+
+  if count == 0:
+    sysTimeUsec = getSysTimeUsec()
+    jackTimeUsec = jacket.getTime().int64
+    sysTimeOffset = sysTimeUsec - jackTimeUsec
+    count = 10
+  else:
+    count -= 1
+  if getCycleTimes(client, currentFrame.addr, currentFrameUsecUnsigned.addr, nextFrameUsecUnsigned.addr, periodUsec.addr) == 0:
+    while keyboardEventBuffer.pop(currentEvent):
+      currentFrameUsec = currentFrameUsecUnsigned.int64
+      nextFrameUsec = nextFrameUsecUnsigned.int64
+      let currentEventUsec = currentEvent.usec.int64 - sysTimeOffset
+      let currentEventFrame = client.timeToFrames(currentEventUsec.uint64)
+      echo [sysTimeOffset, currentFrameUsec, currentEventUsec, currentEventUsec - currentFrameUsec]
+  for i in 0..<64:
+    foo[i] = 0.0
+
+]#
+
+midiWriterClient.deactivate
+midiWriterClient.clientClose
 
 #proc calculateFrame(event: KeyboardEvent, frameZeroTime: Time, numFrames: int): int =
 
