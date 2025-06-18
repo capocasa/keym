a0ce9f538d8953c43894dbc021d368ba246b0d8c
Carlo Capocasa <carlo@capocasa.net>
2025-06-18 12:58:20 +0200

    it works!

--- keym.nim
+++ keym.nim
@@ -6,17 +6,44 @@ type
   RawKeyboardEvent {.packed.} = object
     time: Timeval
     kind: uint16
-    code: uint16
+    scancode: uint16
     value: int32
-  KeyboardEvent {.packed.} = object
+  InputEventKind = enum
+    Note,
+    State
+    Housekeeping
+  StateTransition = enum
+    SemitoneUp,
+    SemitoneDown,
+    OctaveUp,
+    OctaveDown
+  HousekeepingAction = enum
+    AllNotesOff
+  InputEvent {.packed.} = object
     usec: int64
-    note: int8
-    on: bool
+    case kind: InputEventKind
+    of Note:
+      note: int8
+      on: bool
+    of State:
+      transition: StateTransition
+    of Housekeeping:
+      action: HousekeepingAction
 
 let
   EVIOCSCLOCKID* {.importc, header: "<linux/input.h>".}: culong
 
-const latencyPeriods = 2
+const
+  latencyPeriods = 2
+  eventPriority = 98
+  keyboardPath = "/dev/input/event3"
+
+  transposeMin = -60'i8
+  transposeMax = 60'i8
+  
+  noteOn = 0x90'u8
+  noteOff = 0x80'u8
+  control = 0xb0'u8
 
 proc toUsec(t: Timeval): int64 =
   ## older linux kernel time format with microseconds
@@ -41,132 +68,158 @@ proc openDevice(path: string): File =
   var clk = CLOCK_MONOTONIC
   assert ioctl(result.getOSFileHandle, EVIOCSCLOCKID, clk.addr) == 0, "Could not set input subsystem to monotonic time"
 
-proc scancodeToNote(scancode: uint16, note: var int8): bool =
+proc newInputEvent(rawEvent: RawKeyboardEvent): InputEvent =
   # echo "> ", scancode
-  note = case scancode:
-  
+  result = case rawEvent.scancode:
+
   # lower row
   of KEY_Z:
-    60
+    InputEvent(kind: Note, note: 60)
   of KEY_S:
-    61
+    InputEvent(kind: Note, note: 61)
   of KEY_X:
-    62
+    InputEvent(kind: Note, note: 62)
   of KEY_D:
-    63
+    InputEvent(kind: Note, note: 63)
   of KEY_C:
-    64
+    InputEvent(kind: Note, note: 64)
   of KEY_V:
-    65
+    InputEvent(kind: Note, note: 65)
   of KEY_G:
-    66
+    InputEvent(kind: Note, note: 66)
   of KEY_B:
-    67
+    InputEvent(kind: Note, note: 67)
   of KEY_H:
-    68
+    InputEvent(kind: Note, note: 68)
   of KEY_N:
-    69
+    InputEvent(kind: Note, note: 69)
   of KEY_J:
-    70
+    InputEvent(kind: Note, note: 70)
   of KEY_M:
-    71
+    InputEvent(kind: Note, note: 71)
   of KEY_COMMA:
-    72
+    InputEvent(kind: Note, note: 72)
   of KEY_L:
-    73
+    InputEvent(kind: Note, note: 73)
   of KEY_DOT:
-    74
+    InputEvent(kind: Note, note: 74)
   of KEY_SEMICOLON:
-    75
+    InputEvent(kind: Note, note: 75)
   of KEY_SLASH:
-    76
+    InputEvent(kind: Note, note: 76)
 
   # upper row
   of KEY_Q:
-    72
+    InputEvent(kind: Note, note: 72)
   of KEY_2:
-    73
+    InputEvent(kind: Note, note: 73)
   of KEY_W:
-    74
+    InputEvent(kind: Note, note: 74)
   of KEY_3:
-    75
+    InputEvent(kind: Note, note: 75)
   of KEY_E:
-    76
+    InputEvent(kind: Note, note: 76)
   of KEY_R:
-    77
+    InputEvent(kind: Note, note: 77)
   of KEY_5:
-    78
+    InputEvent(kind: Note, note: 78)
   of KEY_T:
-    79
+    InputEvent(kind: Note, note: 79)
   of KEY_6:
-    80
+    InputEvent(kind: Note, note: 80)
   of KEY_Y:
-    81
+    InputEvent(kind: Note, note: 81)
   of KEY_7:
-    82
+    InputEvent(kind: Note, note: 82)
   of KEY_U:
-    83
+    InputEvent(kind: Note, note: 83)
   of KEY_I:
-    84
+    InputEvent(kind: Note, note: 84)
   of KEY_9:
-    85
+    InputEvent(kind: Note, note: 85)
   of KEY_O:
-    86
+    InputEvent(kind: Note, note: 86)
   of KEY_0:
-    87
+    InputEvent(kind: Note, note: 87)
   of KEY_P:
-    88
+    InputEvent(kind: Note, note: 88)
   of KEY_LEFTBRACE:
-    89
+    InputEvent(kind: Note, note: 89)
   of KEY_EQUAL:
-    90
+    InputEvent(kind: Note, note: 90)
   of KEY_RIGHTBRACE:
-    91
+    InputEvent(kind: Note, note: 91)
+  
+  # transpose
+
+  of KEY_LEFT:
+    InputEvent(kind: State, transition: SemitoneDown)
+  of KEY_RIGHT:
+    InputEvent(kind: State, transition: SemitoneUp)
+  of KEY_UP:
+    InputEvent(kind: State, transition: OctaveUp)
+  of KEY_DOWN:
+    InputEvent(kind: State, transition: OctaveDown)
+
+  # safety
+  of KEY_ESC:
+    InputEvent(kind: Housekeeping, action: AllNotesOff)
 
   # ignore other keys
   else:
-    return false
-  true
+    raise newException(ValueError, "unknown key")
 
-const
-  keyboardEventPriority = 96
-  keyboardPath = "/dev/input/event3"
+  result.usec = rawEvent.time.toUsec
+
+  let on = rawEvent.value.bool
+  if result.kind == Note:
+    result.on = on
+  else:
+    if not on:
+      raise newException(ValueError, "keyup ignored")
 
 var
-  keyboardEventThread:Thread[void]
+  eventThread:Thread[void]
   signalThread:Thread[void]
-  keyboardEventBuffer = newRingBuffer[KeyboardEvent](2048)
+  eventBuffer = newRingBuffer[InputEvent](2048)
   terminating = false
   midiPort: Port
   midiWriterClient: Client
   midiWriterStatus: cint
+ 
+
+  # midiwriter State
+  # global for convenience, not thread safe
+  # to create new ways to modify it, use input events
+  # so it is done in the correct order
+  # use only one variable for octave and transpose
+  # because transposing twelve semitones down
+  # is conceptually the same as one octave
+  # (the opposite view exists but is irrelevant in practice)
+  transpose = 0'i8
+
+  # MIDI channel
+  channel = 0'i8
 
 let
   keyboardDevice = openDevice(keyboardPath)
 
-proc keyboardEventHandler() =
+proc eventHandler() =
   while not terminating:
-    var rawKeyboardEvent:RawKeyboardEvent
-    if keyboardDevice.readBuffer(rawKeyboardEvent.addr, sizeof RawKeyboardEvent) != sizeof RawKeyboardEvent:
+    var rawEvent:RawKeyboardEvent
+    if keyboardDevice.readBuffer(rawEvent.addr, sizeof RawKeyboardEvent) != sizeof RawKeyboardEvent:
       # skip oddly-sized reads- should not happen
       continue
-    if rawKeyboardEvent.kind == 1'u16:  # ensure keyboard events only
-      case rawKeyboardEvent.value
+    if rawEvent.kind == 1'u16:  # ensure keyboard events only
+      case rawEvent.value
       of 0'i32, 1'i32:  # press and release only
-        var keyboardEvent:KeyboardEvent
-        keyboardEvent.usec = rawKeyboardEvent.time.toUsec
-        if not scancodeToNote(rawKeyboardEvent.code, keyboardEvent.note):
-          # undefined code, ignore
-          continue
-        keyboardEvent.on = rawKeyboardEvent.value.bool
-        keyboardEventBuffer.push(keyboardEvent)
+        try:
+          eventBuffer.push(newInputEvent(rawEvent))
+        except ValueError:
+          discard
       else:
         # ignore repeat (which would be 2'i32)
         discard
-
-const
-  noteOn = 0x90'u8
-  noteOff = 0x80'u8
 
 proc `[]=`(s: ptr MidiData; i: int8; x: uint8) =
   cast[ptr UncheckedArray[MidiData]](s)[i] = x
@@ -181,40 +234,71 @@ proc midiWriter*(numFrames: NFrames, arg: pointer): cint {.cdecl.} =
     timeOffset = sysTime - jackTime
 
     midiOutBuffer = portGetBuffer(midiPort, numFrames)
-  
-  var
-    currentEvent: KeyboardEvent
+ 
+  midiOutbuffer.midiClearBuffer()
 
-  while keyboardEventBuffer.peek(currentEvent):
+  for event in eventBuffer.pop():
+
+    # calculate sample (jack "frame") for event to occur
+
     let
       frameTime = midiWriterClient.lastFrameTime()
-      nextFrameTime = midiWriterClient.lastFrameTime()
-      currentEventJackTime = currentEvent.usec - timeOffset
-      currentEventFrame = midiWriterClient.timeToFrames(currentEventJackTime.uint64)
-      currentEventFrameFromLastFrameTime = currentEventFrame.int64 - frameTime.int64
+      #nextFrameTime = midiWriterClient.lastFrameTime()
+      eventJackTime = event.usec - timeOffset
+      eventFrame = midiWriterClient.timeToFrames(eventJackTime.uint64)
+      eventFrameFromLastFrameTime = eventFrame.int64 - frameTime.int64
       latency = latencyPeriods * numFrames
     var
-      scheduledFrame = currentEventFrameFromLastFrameTime + latency.int
-    # echo (frameTime, currentEventFrame, currentEventFrameFromLastFrameTime)
+      scheduledFrame = eventFrameFromLastFrameTime + latency.int
+    # echo (frameTime, eventFrame, eventFrameFromLastFrameTime)
     if scheduledFrame < 0:
       scheduledFrame = 0
       stderr.write "Warning: event was late"
     elif scheduledFrame >= numFrames.int:
       break
-    stdout.write $scheduledFrame & " "
-    midiOutbuffer.midiClearBuffer()
-    var data = midiOutBuffer.midiEventReserve(NFrames scheduledFrame, 3)
-    assert not data.isNil, "could not reserve MIDI data"
-    if currentEvent.on:
-      data[0] = noteOn
-      data[1] = currentEvent.note.uint8
-      data[2] = 64'u8  # play nice with other controllers with sensitivity
-    else:
-      data[0] = noteOff
-      data[1] = currentEvent.note.uint8
-      data[2] = 0'u8  # quick release
-    keyboardEventBuffer.readAdvance()
-    echo data[0], " ", data[1], " ", data[2]
+    #stdout.write $scheduledFrame & " "
+
+    case event.kind
+    of Note:
+      var data = midiOutBuffer.midiEventReserve(NFrames scheduledFrame, 3)
+      assert not data.isNil, "could not reserve MIDI data"
+      if event.on:
+        data[0] = noteOn
+        data[1] = (event.note + transpose).uint8
+        data[2] = 64'u8  # half velocity, play nice with other MIDI streams
+      else:
+        data[0] = noteOff
+        data[1] = (event.note + transpose).uint8
+        data[2] = 0'u8  # signify quick release by MIDI conventions
+
+    of State:
+
+      # can just change the state here- scheduled events that rely
+      # on the state have no more use for it at this point because
+      # /dev/input events are guaranteed to arrive in order
+
+      case event.transition:
+      of SemitoneUp:
+        transpose = min(60, transpose + 1)
+      of SemitoneDown:
+        transpose = max(-60, transpose - 1)
+        # leave as is if out of range
+        # (avoid unexpected and hard-to-correct key change that just limiting it would hae)
+      of OctaveUp:
+        transpose += 12
+        if transpose > transposeMax:
+          transpose -= 12  # not thread safe but doesn't need to be
+      of OctaveDown:
+        transpose -= 12
+        if transpose < transposeMin:
+          transpose += 12
+
+    of Housekeeping:
+      var data = midiOutBuffer.midiEventReserve(NFrames scheduledFrame, 3)
+      assert not data.isNil, "could not reserve MIDI data"
+      data[0] = control
+      data[1] = 123'u8
+      data[2] = 0'u8
 
 createThread signalThread, proc() {.thread.} =
   waitSignals(SIGABRT, SIGHUP, SIGINT, SIGQUIT, SIGTERM):
@@ -227,10 +311,12 @@ assert not midiWriterClient.isNil, "Could not create jack client, jack may not b
 midiPort = midiWriterClient.portRegister("out", JackDefaultMidiType, PortIsOutput, 0)
 assert midiWriterClient.setProcessCallback(midiWriter) == 0, "could not set process callback"
 
+eventBuffer.lock()
+
 assert midiWriterClient.activate() == 0, "Could not connect jack"
 
-createRealtimeThread(keyboardEventThread, keyboardEventHandler, priority=98)
-joinThread(keyboardEventThread)  # exit when input thread does, it's simpler not to have to kill it
+createRealtimeThread(eventThread, eventHandler, priority=98)
+joinThread(eventThread)  # exit when input thread does, it's simpler not to have to kill it
 
 midiWriterClient.deactivate
 midiWriterClient.clientClose
